221a9e6b3ed8bde457ff52a66131ee51aba41c0b
margovisuals <damianhebry@gmail.com>
2025-10-15 14:12:09 +0200

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -4,18 +4,17 @@
 // @version      1.0
 // @description  margovisuals
 // @author       kaczka
-// @match        https://*.margonem.pl/*
+// @match        https://*.margonem.pl/
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @icon         https://raw.githubusercontent.com/krystianasaaa/margonem-addons/b939ec05fdd03f6f973cef7a931659c224596bde/ikonka.png
 // @run-at       document-body
-// @updateURL    https://krystianasaaa.github.io/margonem-addons/loader.user.js
-// @downloadURL  https://krystianasaaa.github.io/margonem-addons/loader.user.js
+// @updateURL    https://margovisuals.github.io/margovisuals/loader.user.js
+// @downloadURL  https://margovisuals.github.io/margovisuals/loader.user.js
 // ==/UserScript==
-
 (function() {
     const s = document.createElement("script");
-    s.src = "https://krystianasaaa.github.io/margonem-addons/addons.js?" + Date.now();
+    s.src = "https://margovisuals.github.io/margovisuals/addons.js?" + Date.now();
     document.head.appendChild(s);
 })();
